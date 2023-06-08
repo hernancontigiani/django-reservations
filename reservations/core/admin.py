73d9c4f64e5a4576b04d4d2bfb04b259b04b182c
hernancontigiani <hernan4790@gmail.com>
2023-06-08 08:49:50 -0300

    Config django admin

--- reservations/core/admin.py
+++ reservations/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Property._meta.fields]
+
+
+@admin.register(PricingRule)
+class PricingRuleAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in PricingRule._meta.fields]
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Booking._meta.fields]
