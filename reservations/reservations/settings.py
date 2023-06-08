85f8a2a9f97fec7ef10ae6a68c7ddad8dc834c57
hernancontigiani <hernan4790@gmail.com>
2023-06-08 08:46:32 -0300

    swagger

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'drf_yasg', # swagger
 ]
 
 MIDDLEWARE = [
@@ -50,12 +51,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SWAGGER_SETTINGS = {
+   'SECURITY_DEFINITIONS': {
+   },
+}
+
 ROOT_URLCONF = 'reservations.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- reservations/reservations/urls.py
+++ reservations/reservations/urls.py
@@ -16,7 +16,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# swagger
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+description = '''
+<img src="https://media.licdn.com/dms/image/C4D0BAQEoDcRay1vjng/company-logo_200_200/0/1647283602276?e=2147483647&v=beta&t=mh_4cXfdaPARrbA1_kC_4UqmqjBO-XAH3XWGVZ6fu7k">
+</br>
+</br>
+<h2>APIs Doc</h2>
+'''
+
+schema_view = get_schema_view(
+  openapi.Info(
+     title="Booking system",
+     default_version='1.0.0',
+     description=description,
+  ),
+  public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1.0/', include('core.api.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
