296ddf6819b4d9e48e7317c4b95af5e748d6ddc1
hernancontigiani <hernan4790@gmail.com>
2023-07-17 19:19:52 -0300

    Add serializer custom validation for pricingrule

--- reservations/core/api/serializers.py
+++ reservations/core/api/serializers.py
@@ -6,21 +6,35 @@ class PropertySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Property
-        fields = ('__all__')
+        fields = ('id', 'name', 'base_price')
 
 
 
 class PricingRuleSerializer(serializers.ModelSerializer):
-    property = serializers.PrimaryKeyRelatedField(write_only=True, queryset= Property.objects.all())
+    property = serializers.PrimaryKeyRelatedField(queryset= Property.objects.all())
+
+    def validate(self, data):
+        """
+        Check price rule condition.
+        """
+        if data.get('price_modifier') is None and data.get('fixed_price') is None:
+            raise serializers.ValidationError(
+                {
+                    "price_modifier": "it cannont be null if fixed_price is null",
+                    "fixed_price": "it cannont be null if price_modifier is null"
+                }
+            )
+        return data
 
     class Meta:
         model = PricingRule
-        fields = ('__all__')
+        fields = ('id', 'property', 'price_modifier', 'min_stay_length', 'fixed_price', 'specific_day')
 
     
 class BookingSerializer(serializers.ModelSerializer):
-    property = serializers.PrimaryKeyRelatedField(write_only=True, queryset= Property.objects.all())
+    property = serializers.PrimaryKeyRelatedField(queryset= Property.objects.all())
 
     class Meta:
         model = Booking
-        fields = ('__all__')
+        fields = ('id', 'property', 'date_start', 'date_end', 'final_price')
+
